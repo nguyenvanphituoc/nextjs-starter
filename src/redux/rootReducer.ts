5a60463c748a11b8ea946647e2757a6966bb4c53
liberty <liberty@vinova.com.sg>
2023-09-05 18:13:42 +0700

    fix typescript

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -6,22 +6,4 @@ const appReducer = {
 
 const appCombineReducer = combineReducers(appReducer);
 
-// export type RootState = ReturnType<typeof rootReducer>;
-const rootReducer = (state: typeof appReducer | undefined, action: Action) => {
-  // if (resetStateAction.match(action)) {
-  //   // for all keys defined in your persistConfig(s)
-  //   AsyncStorage.removeItem('persist:root');
-  //   // storage.removeItem('persist:otherKey')
-
-  //   return appCombineReducer(
-  //     {
-  //       [MetaSlice.name]:
-  //         state?.[MetaSlice.name] ?? MetaSlice.getInitialState(),
-  //     },
-  //     action,
-  //   );
-  // }
-  return appCombineReducer(state, action);
-};
-
 export default appCombineReducer;
